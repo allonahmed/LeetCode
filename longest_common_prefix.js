0d711234aed06086e305952237603511fe83c5e7
allonahmed <allonahmed@mec.science>
2022-02-03 22:34:57 -0500

    two sum II

---
+++ longest_common_prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+    @link https://leetcode.com/problems/longest-common-prefix/submissions/
+ */
+
+//96% PROOF REad your code... wasted 10 minutes for being dumb
+var longestCommonPrefix = function (strs) {
+  let results = "";
+  let prefix = "";
+
+  for (let i = 0; i < strs[0].length; i += 1) {
+    results = strs[0].charAt(i);
+    for (let j = 0; j < strs.length; j++) {
+      if (results != strs[j].charAt(i)) {
+        return prefix;
+      }
+    }
+    prefix += results;
+  }
+  return prefix;
+};
